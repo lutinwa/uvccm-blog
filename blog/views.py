35c3a77b621e6cedf42cdfa5ad4b83d9563bc1a0
lennox <lennoxlutinwa@gmail.com>
2022-12-13 17:50:53 +0300

    new files

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def index(request):
+	posts = Post.objects.filter(status=1).order_by('-created_on')
+
+	return render(request, "index.html", {'posts': posts})
+
+def show(request, post_id):
+	post = Post.objects.get(id=post_id)
+
+	return render(request, "show.html", {"post": post})
