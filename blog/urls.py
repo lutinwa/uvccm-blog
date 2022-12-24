d2543ee7c36829e9fad54101aa0da1abf5292d14
Lennox Lutinwa <lennoxlutinwa@gmail.com>
2022-12-24 15:56:09 +0000

    changes

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:post_id>", views.show, name="show")
+    path("<int:post_id>", views.show, name="show"),
     path("contact", views.contact, name="contact"),
     path("uongozi", views.uongozi, name="uongozi"),
     path("malengo", views.malengo, name="malengo"),
